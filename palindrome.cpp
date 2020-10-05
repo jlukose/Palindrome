5b888bb058c66fd17bc144723741e3b017a38d60
jlukose <71291290+jlukose@users.noreply.github.com>
2020-10-04 17:42:25 -0700

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -5,37 +5,39 @@ using namespace std;
 
 int main()
 {
-  char j = 'Z';
-  char str[80];
-  char str2[80];
-  for (int i = 0; i < 80; i++){//makes str and str2 null
-    char str[i] = { '\0' };
-    char str2[i] = { '\0' };
-  }
+  //initialization
+  char ch;
+  char str[80];//input
+  char str2[80];//input with no capitals or symbols
+  char str3[80];//str2 reversed
   int count = 0;
+  for (int i = 0; i < 80; i++){//makes cstrings null
+    str[i] = { '\0' };
+    str2[i] = { '\0' };
+    str3[i] = { '\0' };
+  }
   cin.get(str, 80);
   
-  for (int i = 0; i < 80; i++) {
+  for (int i = 0; i < 80; i++) {//puts the letters in str2
     if (isalpha(str[i])){
       str2[count] = str[i];
       count++;
-      cout << "added " << str[i] << "   count " << count << "   " << str2 << endl;
     }
   }
-
-  cout << str2 << endl << count << endl;
   
   for (int i = 0; i < count; i++) {//makes str2 lowercase
     str2[i] = (tolower(str2[i]));
   }  
 
-  char str3[count];
+  strcpy(str3, str2);//copy str2 to str3
 
-  for (int i = 0; i < count; i++){
-    str3[i] = str2[i];
+  for (int i = 0; i < count - 1; i++){//reverse str3 NOT WORKING
+    ch = str[i];
+    str[i] = str[count - i - 1];
+    str[count - i - 1] = ch;
   }
 
-  cout << str3 << endl;
+  cout << str3;
   
   return 0;
 }
